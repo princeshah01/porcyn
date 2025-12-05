cf9489b72135a9a52434f7133a0b2faaf14de6e9
Prince Shah <Prince.rjb839@gmail.com>
2025-12-05 08:30:10 +0530

    change theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="">Welcome to My Next.js App</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="font-inter">Welcome to My Next.js App</h1>
+      <p className="font-space-grotesk">
+        This is a simple Next.js application.
+      </p>
     </div>
   );
 }
